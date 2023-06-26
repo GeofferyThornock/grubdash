4d299a2a62f08c03bca976bae4acefca4d046952
GeofferyThornock <geofferythornock@gmail.com>
2023-06-26 12:52:21 -0500

    finalized the dishes route finishing update

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -23,13 +23,20 @@ function makeValidatorFor(field) {
 
 function checkPriceValidator(req, res, next) {
     const { price } = req.body.data;
-    if (price <= 0 || isNaN(price)) {
+    if (price) {
+        if (price <= 0 || typeof price !== "number") {
+            next({
+                status: 400,
+                message: `price is either 0 or is not a number`,
+            });
+        } else {
+            next();
+        }
+    } else {
         next({
             status: 400,
             message: `price is either 0 or is not a number`,
         });
-    } else {
-        next();
     }
 }
 
@@ -67,6 +74,27 @@ function read(req, res, next) {
     res.status(200).json({ data: res.locals.dish });
 }
 
+function update(req, res, next) {
+    const { name, description, price, image_url } = req.body.data;
+    const dish = res.locals.dish;
+
+    if (req.body.data.id) {
+        if (dish.id !== req.body.data.id) {
+            return next({
+                status: 400,
+                message: `Dish id does not match route id. Dish: ${req.body.data.id}, Route: ${dish.id}`,
+            });
+        }
+    }
+
+    dish.name = name;
+    dish.description = description;
+    dish.price = price;
+    dish.image_url = image_url;
+
+    res.send({ data: dish });
+}
+
 module.exports = {
     list,
     create: [
@@ -75,4 +103,10 @@ module.exports = {
         create,
     ],
     read: [urlExists, read],
+    update: [
+        urlExists,
+        ...["name", "description", "image_url"].map(makeValidatorFor),
+        checkPriceValidator,
+        update,
+    ],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -9,6 +9,10 @@ router
     .post(controller.create)
     .all(methodNotAllowed);
 
-router.route("/:dishId").get(controller.read).all(methodNotAllowed);
+router
+    .route("/:dishId")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed);
 
 module.exports = router;
