6351f038d3268251c99db7dd89b297de0cd6ea9c
GeofferyThornock <geofferythornock@gmail.com>
2023-06-26 11:02:15 -0500

    finished create and list

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,52 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function makeValidatorFor(field) {
+    return function (req, res, next) {
+        if (req.body.data[field]) {
+            next();
+        } else {
+            next({
+                status: 400,
+                message: `Request body must include ${field}`,
+            });
+        }
+    };
+}
+
+function checkPriceValidator(req, res, next) {
+    const { price } = req.body.data;
+    if (price === 0 || isNaN(price)) {
+        next({
+            status: 400,
+            message: `Price is either 0 or is not a number`,
+        });
+    } else {
+        next();
+    }
+}
+// Validators end
+
+function create(req, res, next) {
+    const { name, description, price, image_url } = req.body.data;
+
+    const newDish = { id: nextId, name, description, price, image_url };
+
+    dishes.push(newDish);
+
+    res.status(201).json({ data: newDish });
+}
+
+function list(req, res, next) {
+    res.status(200).json({ data: dishes });
+}
+
+module.exports = {
+    list,
+    create: [
+        ...["name", "description", "image_url"].map(makeValidatorFor),
+        checkPriceValidator,
+        create,
+    ],
+};
