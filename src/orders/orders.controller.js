410a80b6d3104be3419e653d5d43e8687f00dace
GeofferyThornock <geofferythornock@gmail.com>
2023-06-26 14:43:00 -0500

    finished create functionality

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,79 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+function validator(field) {
+    return function (req, res, next) {
+        if (req.body.data[field]) {
+            next();
+        } else {
+            next({
+                status: 400,
+                message: `Request body must include ${field}`,
+            });
+        }
+    };
+}
+
+function dishValidator(req, res, next) {
+    if (req.body.data.dishes && req.body.data.dishes.length) {
+        const dishes = req.body.data.dishes;
+        if (Array.isArray(dishes)) {
+            dishes.forEach((e, index) => {
+                if (e.quantity <= 0) {
+                    next({
+                        status: 400,
+                        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+                    });
+                } else if (typeof e.quantity !== "number") {
+                    next({
+                        status: 400,
+                        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+                    });
+                }
+            });
+            next();
+        } else {
+            next({
+                status: 400,
+                message: `Order must include at least one dish`,
+            });
+        }
+    } else {
+        next({
+            status: 400,
+            message: `Order must include a dish`,
+        });
+    }
+}
+
+//Validator functions end
+
+function list(req, res, next) {
+    res.status(200).json({ data: orders });
+}
+
+function create(req, res, next) {
+    const { deliverTo, mobileNumber, status, dishes } = req.body.data;
+
+    const newOrder = {
+        id: nextId(),
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes,
+    };
+
+    orders.push(newOrder);
+
+    res.status(201).json({ data: newOrder });
+}
+
+module.exports = {
+    list,
+    create: [
+        ...["deliverTo", "mobileNumber"].map(validator),
+        dishValidator,
+        create,
+    ],
+};
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,12 @@
+const methodNotAllowed = require("../utils/methodNotAllowed");
+const controller = require("./orders.controller");
 const router = require("express").Router();
 
 // TODO: Implement the /orders routes needed to make the tests pass
+router
+    .route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
 
 module.exports = router;
